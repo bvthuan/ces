50c1719d639d2f06bc9c20499604322c64ca5860
viet.tran <vitra@netcompany.com>
2019-05-16 21:33:32 +0700

    find route

---
+++ CES/Extension/Parser.cs
@@ -0,0 +1,50 @@
+﻿using CES.Database.Models;
+using CES.Enums;
+using CES.Models.FindPath;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CES.Extension
+{
+	public static class Parser
+	{
+		public static Node GetNode(this List<Node> Nodes, string name)
+		{
+			return Nodes.First(n => n.Name == name);
+		}
+
+		public static List<Node> Parse(this List<RouteModel> Routes, TransportType TransportType, decimal price)
+		{
+			var Nodes = new List<Node>();
+			foreach (var item in Routes)
+			{
+				if (Nodes.Any(n => n.Name == item.Start)) continue;
+
+				var node = new Node() { Name = item.Start };
+				Nodes.Add(node);
+			}
+
+			foreach (var item in Nodes)
+			{
+				var connectedRoutes = Routes.Where(r => r.Start == item.Name).ToList();
+
+				foreach (var edge in connectedRoutes)
+				{
+					var newEdge = new Edge
+					{
+						Cost = TransportType == TransportType.Fastest 
+							? edge.TotalHours
+							: edge.NumberOfSegments * price,
+						Price = edge.NumberOfSegments * price,
+						Time = edge.TotalHours,
+						ConnectedNode = Nodes.First(n => n.Name == edge.Destination),
+						Transportation = edge.Transportation
+					};
+					item.Connections.Add(newEdge);
+				}
+			}
+
+			return Nodes;
+		}
+	}
+}
---
+++ CES/Models/FindPath/Node.cs
@@ -0,0 +1,54 @@
+﻿using CES.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CES.Models.FindPath
+{
+	public class Node
+	{
+		public Guid Id { get; set; }
+		public string Name { get; set; }
+		public List<Edge> Connections { get; set; } = new List<Edge>();
+		public decimal MinCostToStart { get; set; }
+		public Node NearestToStart { get; set; }
+		
+		public bool Visited { get; set; }
+		public double StraightLineDistanceToEnd { get; set; }
+		public bool IsEnd { get; set; }
+		public override string ToString()
+		{
+			return Name;
+		}
+	}
+
+	public class Edge
+	{
+		/// <summary>
+		/// Cost for fastest path
+		/// </summary>
+		public decimal Cost { get; set; }
+		public Node ConnectedNode { get; set; }
+		/// <summary>
+		/// Price
+		/// </summary>
+		public decimal Price { get; set; }
+		public override string ToString()
+		{
+			return "-> " + ConnectedNode.ToString();
+		}
+
+		public Transportation Transportation { get; set; }
+		public int Time { get; set; }
+	}
+
+	public class RouteModel
+	{
+		public string Start { get; set; }
+		public string Destination { get; set; }
+		public int NumberOfSegments { get; set; }
+		public int TotalHours { get; set; }
+		public Transportation Transportation { get; set; }
+	}
+}
