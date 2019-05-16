9b2449f58be9f50be975e4c462ba1d6372216bb0
Bach Van Thuan <thuan.bach@netcompany.com>
2019-05-16 13:43:57 +0700

    add city controller

---
+++ CES/Services/CityService.cs
@@ -0,0 +1,27 @@
+﻿using CES.Database.Context;
+using CES.Database.Models;
+using CES.Extension;
+using CES.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CES.Services
+{
+	public class CityService : ICity
+	{
+		public CesContext _context;
+		public CityService(CesContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<List<City>> Cities()
+		{
+			return await _context.Cities.ToListAsync();
+		}
+
+	}
+}
