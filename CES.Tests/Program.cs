642517cb978006bfaa991232ded02f7eaed6911c
Mike <vudinhtoan2990@gmail.com>
2019-05-17 00:12:20 +0700

    init test

---
+++ CES.Tests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CES.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ UnitTestProject1/Integration/FastestRouteTest.cs
@@ -0,0 +1,22 @@
+﻿using CES.Model.Request;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CES.Tests.Integration
+{
+    [TestClass]
+    class FastestRouteTest
+    {
+        [TestMethod]
+        public void TestFindFastestRouteByCar()
+        {
+            RouteRequestModel request = new RouteRequestModel()
+            {
+                Start = "VIS",
+                Destination = "DAK"
+            };
+        }
+    }
+}
